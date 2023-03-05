1cecd84141a6c8d35ccc1ffd9f61ab4332253da7
Travis Staloch <twostepted@gmail.com>
2023-03-05 10:46:26 -0800

    use new addModule api

--- build.zig
+++ build.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{
-        .name = "tres",
-        .source_file = .{ .path = "tres.zig" },
-    });
+    _ = b.addModule("tres", .{ .source_file = .{ .path = "tres.zig" } });
 }
