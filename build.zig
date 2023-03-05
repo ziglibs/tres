707a09313b42e05d6ae22d1590499eece5f968ce
Yuri Pieters <MageJohn@users.noreply.github.com>
2023-03-05 18:53:37 +0000

    Use new addModule syntax (#9)
    Co-authored-by: Yuri Pieters <yuri@zopatista.com>

--- build.zig
+++ build.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    b.addModule(.{
-        .name = "tres",
+    _ = b.addModule("tres", .{
         .source_file = .{ .path = "tres.zig" },
     });
 }
